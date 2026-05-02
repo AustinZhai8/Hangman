d84ea42b7ed2af19cf511d789d5e1b980c9812c1
AustinZhai8 <austinhzhai@gmail.com>
2026-05-02 03:06:25 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import random
+
+from randomWords import easyWordlist, mediumWordlist, hardWordlist
+
+def chooseDificulty():
+    while True:
+        difficulty = input('Enter the difficulty of the word (easy, medium or hard): ').lower()
+        if difficulty == "easy":
+            return random.choice(easyWordlist)
+        elif difficulty == "medium":
+            return random.choice(mediumWordlist)
+        elif difficulty == "hard":
+            return random.choice(hardWordlist)
+        else:
+            print ('Invalid response please try again')
+
+
+lives = 6
+guessedLetters = []
+guessedWords = []
+correct = False
+
+print('\n--------Welcome to Hangman!--------\n')
+word = chooseDificulty()
+
+lettersinWord = '_' * len(word)
+
+print(f'\nYou have {lives} lives left')
+print(lettersinWord)
+
+while not correct and lives > 0:
+
+    guess = input('Enter your guess: ').lower()
+
+    if len(guess) == 1 and guess.isalpha():
+        if guess in guessedLetters:
+            print(f'You already guessed the letter {guess}')
+        elif guess in word:
+            print(f'Good job, {guess} is in the word!')
+            guessedLetters.append(guess)
+            wordAsList = list(lettersinWord)
+            for i in range(len(word)):
+                if word[i] == guess:
+                    wordAsList[i] = guess
+            lettersinWord = ''.join(wordAsList)
+            if '_' not in lettersinWord:
+                correct = True
+        else:
+            print(f'{guess} is not in the word')
+            lives -= 1
+            guessedLetters.append(guess)
+
+    elif len(guess) == len(word) and guess.isalpha():
+        if guess in guessedWords:
+            print(f'You already guessed the word {guess}')
+        elif guess != word:
+            print(f'{guess} is not the word')
+            lives -= 1
+            guessedWords.append(guess)
+        else:
+            correct = True
+            lettersinWord = word
+    else:
+        print('Invalid guess, try again')
+
+    print(f'\nYou have {lives} lives left')
+    print(lettersinWord)
+
+if correct and lives > 0:
+    print(f'Congratulations you guessed the word {word}!')
+
+else:
+    print(f'Sorry you ran out of lives the word was {word}')
---
+++ randomWords.py
@@ -0,0 +1,28 @@
+easyWordlist = [
+    "egg", "toy", "water", "day", "shower", "tiger", "home", "coat",
+    "garden", "rise", "throw", "crown", "baby", "office", "beach",
+    "phone", "computer", "flower", "bank", "train", "brush", "pole", "game",
+    "bug", "code", "loop", "list", "file", "data", "test", "link",
+    "text", "font", "grid", "node", "port", "site", "wire", "byte",
+]
+
+mediumWordlist = [
+    "soup", "tree", "orange", "mango", "coding", "program", "uncle",
+    "aunty", "inspiration", "traffic", "hallway", "secure", "general",
+    "brick", "absence", "haircut", "bottle", "host",
+    "python", "syntax", "variable", "function", "software", "hardware",
+    "network", "internet", "database", "debugging", "developer", "boolean",
+    "integer", "runtime", "hosting", "backend", "frontend", "request",
+    "response", "library", "package",
+]
+
+hardWordlist = [
+    "uncanny", "ironic", "literally", "procrastination", "endure",
+    "advocate", "baffle", "barren", "devote", "fathom", "gibberish",
+    "hostile", "junk", "kudos", "lunatic", "mundane", "obscure",
+    "omen", "quantum", "pseudonym", "quench", "solace", "tedious",
+    "algorithm", "encryption", "recursion", "inheritance", "polymorphism",
+    "abstraction", "middleware", "asynchronous", "serialization", "authentication",
+    "authorization", "refactoring", "compilation", "interpreter", "dependency",
+    "deployment", "containerization", "microservice", "concurrency", "repository",
+]
